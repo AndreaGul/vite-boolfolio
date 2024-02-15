fa9b3f6861fd0728ab2f06489465f4feb9723314
AndreaGul <andreagulino99@gmail.com>
2024-02-15 21:58:20 +0100

    feat:milestone 3

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,25 @@ import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 export default {
   name: 'Template',
+  data() {
+    return {
+      baseUrl: 'http://127.0.0.1:8000/',
+      apiUrls: {
+        project: 'api/project',
+      },
+    };
+  },
   components: {
     AppHeader,
     AppMain,
     AppFooter,
   },
+  methods: {
+    getProjects() {
+      axios.get();
+    },
+  },
+  created() {},
 };
 </script>
 
