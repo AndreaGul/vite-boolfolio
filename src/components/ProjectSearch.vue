06a9c8d60c9d27717900a572430ed74eee0b5217
AndreaGul <andreagulino99@gmail.com>
2024-02-19 21:14:22 +0100

    feat: input di ricerca in pagina e conservasione degli errori se si inseriscono meno di 3 caratteri

---
+++ src/components/ProjectSearch.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios';
+import store from '../store';
+export default {
+  name: 'ProjectSearch',
+  data() {
+    return {
+      store,
+    };
+  },
+  emits: ['searchProject'],
+  methods: {
+    search() {
+      console.log('sto cercando qualcosa');
+      this.$emit('searchProject');
+    },
+  },
+};
+</script>
+<template>
+  <section id="project-search">
+    <form action="" @submit.prevent="search">
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="search-key"
+          placeholder="Nome progetto"
+          v-model="store.pages.searchKey"
+        />
+        <label for="search-key" class="form-label">Nome progetto</label>
+      </div>
+      <button class="btn btn-primary">Cerca</button>
+    </form>
+  </section>
+</template>
--- src/pages/ListProject.vue
+++ src/pages/ListProject.vue
@@ -1,29 +1,39 @@
 <script>
 import axios from 'axios';
 import ProjectCard from '../components/ProjectCard.vue';
-
+import ProjectSearch from '../components/ProjectSearch.vue';
+import store from '../store';
 export default {
   name: 'ListProject',
   data() {
     return {
-      projects: [],
-      baseUrl: 'http://127.0.0.1:8000/',
-      apiUrls: {
-        projects: 'api/project',
-      },
+      error: [],
+      projects: {},
+      store,
     };
   },
-  components: { ProjectCard },
+  components: { ProjectCard, ProjectSearch },
   methods: {
     getProjects() {
       axios
-        .get(this.baseUrl + this.apiUrls.projects)
+        .get(this.store.api.baseUrl + this.store.api.apiUrls.projects, {
+          params: {
+            key: this.store.pages.searchKey,
+          },
+        })
         .then((response) => {
+          console.log('risposta prima');
           console.log(response);
-          this.projects = response.data.data;
+          console.log('risposta dopo');
+          this.projects = response.data;
+          console.log(this.projects.results.data);
         })
         .catch((error) => {
           console.log(error);
+          this.projects.results.data = [];
+          this.error = error.response.data.message;
+          console.log(this.projects.results.data);
+          console.log(this.error);
         });
     },
   },
@@ -35,8 +45,9 @@ export default {
 
 <template>
   <main class="container">
+    <ProjectSearch @search-project="getProjects" />
     <ul class="row list-unstyled">
-      <li class="col-sm-4 g-3" v-for="project in projects">
+      <li class="col-sm-4 g-3" v-for="project in projects.results">
         <ProjectCard :project="project" />
       </li>
     </ul>
